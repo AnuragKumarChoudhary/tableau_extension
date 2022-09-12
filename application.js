15524ab8a6cda572ecdbe7d443fa43e0b5c4a977
AnuragKumarChoudhary <anuragkumarchoudhary@outlook.com>
2022-09-12 13:15:19 +0530

    Initial Commit

---
+++ application.js
@@ -0,0 +1,20 @@
+'use strict';
+ 
+(function () {
+  $(document).ready(function () {
+    // Initialises Tableau Data Extension
+    tableau.extensions.initializeAsync().then(function () {
+        refresh();
+    }, function () { console.log('Error while Initializing: ' + err.toString()); });
+
+    $('#simple-example-table').cardtable();
+  });
+ 
+  function refresh() {
+    // Gets a list of the worksheets and adds them to the web page.
+    $("#worksheets").text("");
+    tableau.extensions.dashboardContent.dashboard.worksheets.forEach(function (worksheet) {
+        $("#worksheets").append("<button class='btn btn-secondary btn-block'>"+worksheet.name+"</button>");
+    });
+  }
+})();
